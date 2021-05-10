098304c65e0fcfee33d634c6d07cac3a6c2fe59d
shivani7335 <shivani7335@gmail.com>
2021-05-10 13:38:41 +0530

    All changes

---
+++ AsIndiaMovieRatingApp/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AsIndiaMovieRatingApp.Models;
+
+namespace AsIndiaMovieRatingApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private AsIndiaEntities db = new AsIndiaEntities();
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View("Login");
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            var count = db.Accounts.Count(a => a.Username.Equals(username) && a.Password.Equals(password));
+            if(count>0)
+            {
+                Session["username"] = username;
+                return RedirectToAction("Index", "Movie");
+            }
+            else
+            {
+                ViewBag.error = "Invalid Access !";
+                return View("Login");
+            }
+        }
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            return View("SignUp");
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(Account account)
+        {
+            db.Accounts.Add(account);
+            db.SaveChanges();
+            return RedirectToAction("Login","Account");
+        }
+    }
+}
+
+
+
+
+
---
+++ AsIndiaMovieRatingApp/Controllers/MovieController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AsIndiaMovieRatingApp.Models;
+
+namespace AsIndiaMovieRatingApp.Controllers
+{
+    public class MovieController : Controller
+    {
+        private AsIndiaEntities db = new AsIndiaEntities();
+
+        public ActionResult Index()
+        {
+            ViewBag.movies = db.Movies.ToList();
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Detail(int id)
+        {
+            var movie = db.Movies.Find(id);
+            ViewBag.movie = movie;
+            var review = new Review()
+            {
+                MovieId = movie.Id
+            };
+            return View("Detail", review);
+        }
+        [HttpPost]
+       public ActionResult SendReview(Review review , double rating)
+        {
+            string username = Session["username"].ToString();
+            review.DatePost = DateTime.Now;
+            review.UserId = db.Accounts.Single(a => a.Username.Equals(username)).Id;
+            review.Rating = rating;
+            db.Reviews.Add(review);
+            db.SaveChanges();
+            return RedirectToAction("Detail", "Movie" , new { id = review.MovieId});
+        }
+    }
+}
